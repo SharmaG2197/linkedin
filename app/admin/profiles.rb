f8b4d88dd694fc80292dd7f1b33e14055db81963
sharmag2101 <deepak@simplyalliancess.com>
2024-05-12 19:54:41 +0530

    'fix_ui_design'

--- app/admin/profiles.rb
+++ app/admin/profiles.rb
@@ -1,18 +1,50 @@
 ActiveAdmin.register Profile do
+  permit_params :username, :address, :dob, :about, :bio, :user_id, :profile_pic
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :username, :address, :dob, :about, :bio, :user_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:username, :address, :dob, :about, :bio, :user_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  action_item 'Back', only: :show do
+    link_to('Back', :back)
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :username
+    column :email do |profile|
+      profile.user.email
+    end
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :username
+      row :address
+      row :dob
+      row :about
+      row :bio
+      row :email do |profile|
+        profile.user.email
+      end
+      row 'Profile Image' do |profile|
+        if profile.profile_pic.attached?
+          image_tag url_for(profile.profile_pic), style: 'width: 150px'
+        else
+          'No image available'
+        end
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :username
+      f.input :address
+      f.input :dob
+      f.input :about
+      f.input :bio
+      f.input :user, as: :select, collection: User.pluck(:email, :id)
+      f.input :profile_pic, as: :file, hint: f.object.profile_pic.attached? ? image_tag(f.object.profile_pic.variant(resize_to_limit: [100, 100])) : content_tag(:span, "No Image Attached")
+    end
+    f.actions
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,8 @@
 class Message < ApplicationRecord
-    belongs_to :sender, class_name: 'User'
-    belongs_to :receiver, class_name: 'User'
+  belongs_to :sender, class_name: 'User'
+  belongs_to :receiver, class_name: 'User'
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "receiver_id", "sender_id", "text", "updated_at"]
+  end
 end
